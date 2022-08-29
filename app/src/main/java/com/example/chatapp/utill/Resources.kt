aab8721f3960cbb3cb96cead32135df239c270cc
Charlie Chale <chalemcchale98@gmail.com>
2022-08-29 12:22:45 +0300

    message services, resources

---
+++ app/src/main/java/com/example/chatapp/utill/Resources.kt
@@ -0,0 +1,6 @@
+package com.example.chatapp.utill
+
+sealed class Resources<T>(val data: T? = null, val message: String? = null) {
+    class Success<T>(data: T): Resources<T>(data)
+    class Error<T>(message: String?, data: T? = null): Resources<T>(data,message)
+}
